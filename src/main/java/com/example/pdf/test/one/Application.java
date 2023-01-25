7b4fe59bc775a62c0c6e576ff979a80b84be2523
jjorgeaugusto <74388566+jjorgeaugusto@users.noreply.github.com>
2023-01-25 20:51:21 -0300

    update new class

--- src/main/java/com/example/pdf/test/one/Application.java
+++ src/main/java/com/example/pdf/test/one/Application.java
@@ -1,12 +1,25 @@
 package com.example.pdf.test.one;
 
+import com.google.common.collect.Table;
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.border.Border;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -23,10 +36,42 @@ public class Application {
 
 		document.open();
 		Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-		Chunk chunk = new Chunk("Teste Gerador", font);
+		Chunk chunk = new Chunk("Teste Gerador de PDF", font);
 
 		document.add(chunk);
+//		document.close();
+
+		// Adicionando Imagem
+		document.addTitle("Primeiro Teste De Titulo");
+
+		File file = new File("cachorro.jpg");
+
+		Image img = null;
+		try {
+			img = Image.getInstance(file.toURI().toURL());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		img.scaleAbsoluteWidth(50);
+		img.scaleAbsoluteHeight(50);
+		document.add(img);
+
+		PdfPTable table = ControllerTable.criarCabecalho();
+
+		List<Pessoa> lpessoa = new ArrayList<Pessoa>();
+		Pessoa pessoa1 = new Pessoa("Jorge","28/04/1997",'M');
+		Pessoa pessoa2 = new Pessoa("Indiano","23/01/1977",'M');
+		Pessoa pessoa3 = new Pessoa("Estranho","01/04/1966",'F');
+
+		lpessoa.add(pessoa1);
+		lpessoa.add(pessoa2);
+		lpessoa.add(pessoa3);
+
+		ControllerTable.preencherDados(document, table, lpessoa);
 		document.close();
+
+
+//
 	}
 
 }
